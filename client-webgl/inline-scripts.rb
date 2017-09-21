62d521fdc7276d1554b315ccf3c4a82e09e5b9d1
Prophesi <prophesi@Rogue.local>
2017-09-21 11:01:38 -0400

    take out gzipping temporarily

--- client-webgl/inline-scripts.rb
+++ client-webgl/inline-scripts.rb
@@ -4,13 +4,14 @@
 html = open('source.html') { |f| f.read }
 html.gsub!(%r{<script src="(js\.libs/)?[^/"]+"></script>}) do |tag|
   js = tag.match(/(?<=").+(?=")/)[0]
-  src = if js.match(/\.min\./)
-    open(js) { |f| f.read }
-  else
-    `java -jar ~/bin/closure-compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js '#{js}'`
-  end
+  src = open(js) { |f| f.read }
+#  src = if js.match(/\.min\./)
+#    open(js) { |f| f.read }
+#  else
+#    `java -jar ~/bin/closure-compiler.jar --compilation_level SIMPLE_OPTIMIZATIONS --js '#{js}'`
+#  end
   "<script> // #{js}\n#{src.strip}\n</script>" 
 end
 target = '../deploy/index.html'
 open(target, 'w') { |f| f.write(html) }
-`gzip --best --no-name --force #{target}`
+#`gzip --best --no-name --force #{target}`
